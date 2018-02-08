9bcfc57723efe0d4f2740da5cc2c856e62ff5eb2
Jesus-Sto <34258205+Jesus-Sto@users.noreply.github.com>
2018-02-08 13:25:49 -0600

    Add files via upload

---
+++ WENDYS_FINAL/APP_INTELCOMERCIAL_DM.DISTRIBUTOR_ACCOUNT_BT_ST.sql
@@ -0,0 +1,88 @@
+CREATE VIEW APP_INTELCOMERCIAL_DM.DISTRIBUTOR_ACCOUNT_BT_ST
+-- AUTHOR:     GERARDO GÓMEZ
+-- DATE:       DICEMBER, 2017
+-- OBJETIVE:   FINAL INFORMATION VIEW
+AS LOCKING ROW FOR ACCESS
+
+SELECT 
+      a.BillToNumber
+     ,b.BillToNumber2          
+     ,b.BillToName1
+     ,b.BillToAddress1
+     ,b.BillToAddress2
+     ,b.BillToCity
+     ,b.BillToStateProv
+     ,b.BillToZipCode 
+     ,b.BillToCountry                
+     ,a.ShipToNumber          
+     ,NULL AS ShipToSiteNumber
+     ,a.ShipToName1
+     ,a.ShipToAddress1
+     ,a.ShipToAddress2
+     ,a.ShipToCity
+     ,a.ShipToStateProv
+     ,a.ShipToZipCode 
+     ,a.ShipToCountry                
+FROM
+     (
+          SELECT 
+                HCSUA.LOCATION                         AS BILLTONUMBER
+               ,HCA.ACCOUNT_NUMBER                     AS SHIPTONUMBER          
+               ,COALESCE(HCA.ACCOUNT_NAME,'UNASSIGNED')AS SHIPTONAME1
+               ,HLO.ADDRESS1                           AS SHIPTOADDRESS1
+               ,HLO.ADDRESS2                           AS SHIPTOADDRESS2
+               ,HLO.CITY                               AS SHIPTOCITY
+               ,HLO.PROVINCE                           AS SHIPTOSTATEPROV
+               ,HLO.POSTAL_CODE                        AS SHIPTOZIPCODE 
+               ,HPB.COUNTRY                            AS SHIPTOCOUNTRY                
+          FROM DWH_OPERERP_GLOBAL.HZ_CUST_ACCOUNTS HCA                       
+                ,DWH_OPERERP_GLOBAL.HZ_CUST_ACCT_SITES_ALL      HCASA             
+                ,DWH_OPERERP_GLOBAL.HZ_PARTIES HPB                                
+                ,DWH_OPERERP_GLOBAL.HZ_PARTIES HPR                                
+                ,DWH_OPERERP_GLOBAL.HZ_PARTIES HPN                                
+                ,DWH_OPERERP_GLOBAL.HZ_PARTY_SITES PS       
+                ,DWH_OPERERP_GLOBAL.HZ_CUST_SITE_USES_ALL HCSUA      
+                ,DWH_OPERERP_GLOBAL.HZ_LOCATIONS HLO     
+               WHERE HCA.PARTY_ID = HPB.PARTY_ID                                  
+                 AND HCA.PARTY_ID = HPR.PARTY_ID                                  
+                 AND HCA.PARTY_ID = HPN.PARTY_ID                                  
+                 AND HCA.PARTY_ID = PS.PARTY_ID                                   
+                 AND HCA.CUST_ACCOUNT_ID = HCASA.CUST_ACCOUNT_ID                                   
+                 AND HCASA.CUST_ACCT_SITE_ID = HCSUA.CUST_ACCT_SITE_ID 
+                 AND HCASA.ORG_ID = HCSUA.ORG_ID      
+                 AND PS.PARTY_SITE_ID = HCASA.PARTY_SITE_ID     
+                 AND PS.LOCATION_ID = HLO.LOCATION_ID     
+                 AND HCASA.ORG_ID IN (SELECT ORGANIZATION_ID FROM STG_CANADA_VIEWS.V_PLANTS WHERE LEGALENTITY_ID IN ('191') GROUP BY 1 )     
+     )A
+     INNER JOIN
+               (
+                    SELECT 
+                          HCSUA.LOCATION                         AS BILLTONUMBER
+                         ,HCA.ACCOUNT_NUMBER                     AS BILLTONUMBER2          
+                         ,COALESCE(HCA.ACCOUNT_NAME,'UNASSIGNED')AS BILLTONAME1
+                         ,HLO.ADDRESS1                           AS BILLTOADDRESS1
+                         ,HLO.ADDRESS2                           AS BILLTOADDRESS2
+                         ,HLO.CITY                               AS BILLTOCITY
+                         ,HLO.PROVINCE                           AS BILLTOSTATEPROV
+                         ,HLO.POSTAL_CODE                        AS BILLTOZIPCODE 
+                         ,HPB.COUNTRY                            AS BILLTOCOUNTRY                
+                    FROM DWH_OPERERP_GLOBAL.HZ_CUST_ACCOUNTS HCA                       
+                          ,DWH_OPERERP_GLOBAL.HZ_CUST_ACCT_SITES_ALL      HCASA             
+                          ,DWH_OPERERP_GLOBAL.HZ_PARTIES HPB                                
+                          ,DWH_OPERERP_GLOBAL.HZ_PARTIES HPR                                
+                          ,DWH_OPERERP_GLOBAL.HZ_PARTIES HPN                                
+                          ,DWH_OPERERP_GLOBAL.HZ_PARTY_SITES PS       
+                          ,DWH_OPERERP_GLOBAL.HZ_CUST_SITE_USES_ALL HCSUA      
+                          ,DWH_OPERERP_GLOBAL.HZ_LOCATIONS HLO     
+                         WHERE HCA.PARTY_ID = HPB.PARTY_ID                                  
+                           AND HCA.PARTY_ID = HPR.PARTY_ID                                  
+                           AND HCA.PARTY_ID = HPN.PARTY_ID                                  
+                           AND HCA.PARTY_ID = PS.PARTY_ID                                   
+                           AND HCA.CUST_ACCOUNT_ID = HCASA.CUST_ACCOUNT_ID                                   
+                           AND HCASA.CUST_ACCT_SITE_ID = HCSUA.CUST_ACCT_SITE_ID 
+                           AND HCASA.ORG_ID = HCSUA.ORG_ID      
+                           AND PS.PARTY_SITE_ID = HCASA.PARTY_SITE_ID     
+                           AND PS.LOCATION_ID = HLO.LOCATION_ID     
+                           AND HCASA.ORG_ID IN (SELECT ORGANIZATION_ID FROM STG_CANADA_VIEWS.V_PLANTS WHERE LEGALENTITY_ID IN ('191') GROUP BY 1 )     
+               )B ON A.SHIPTONUMBER = B.BILLTONUMBER
+WHERE SHIPTONAME1 LIKE '%WENDY%';
---
+++ WENDYS_FINAL/APP_INTELCOMERCIAL_DM.DISTRIBUTOR_ACCOUNT_VF.sql
@@ -0,0 +1,39 @@
+CREATE VIEW IF NOT EXISTS APP_INTELCOMERCIAL_DM.DISTRIBUTOR_ACCOUNT_VF
+-- AUTHOR:     GERARDO G�MEZ
+-- DATE:       DICEMBER, 2017
+-- MODIFIED:   FEBRUARY, 2018
+-- OBJETIVE:   FINAL INFORMATION VIEW
+-- OBJETIVE:   ADD QUEBEC
+AS
+
+
+SELECT
+       Customer_ID                  AS ShipTo
+      ,Doc_Number                   AS InvoiceNumber
+     ,Bill_Num                          AS LineNumber
+     ,'  '                                       AS CreditMemoNumber
+     ,Ship_To                            AS DistributorCustomerID
+     ,Sales_Center                    AS DistributorCenterID
+     ,Product_ID                        AS DistributorProductID
+     ,'  '                                       AS GTIN
+     ,'  '                                       AS SellUOM
+     ,Bill_Date                            AS InvoiceDate
+     ,'  '                                       AS OrderDate
+     ,'  '                                       AS ShippingDate
+     ,'  '                                       AS ScheduledDeliveryDate
+     ,'  '                                       AS ActualDeliveryDate
+     ,Net_Invoice_Qty               AS OrderQuantity
+     ,Gross_Invoice_Qty           AS OriginalShippedQuantity
+     ,Price                                 AS UnitPrice
+     ,Total_Sales                       AS ExtendedPrice
+FROM STG_DSD_CANADA.T_ORDER_FINAL
+
+WHERE Bill_Date >= TO_DATE(DATE_SUB(FROM_UNIXTIME(UNIX_TIMESTAMP()),1))
+AND CUSTOMER_ID IN
+                    (
+                         SELECT Customer_ID FROM tmp_ic.customers_by_route_all_test
+                         WHERE CUSTOMER_NAME LIKE '%WENDY%'
+                         GROUP BY 1
+                    )
+AND SALES_CENTER  NOT IN ('-1')
+GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18;
