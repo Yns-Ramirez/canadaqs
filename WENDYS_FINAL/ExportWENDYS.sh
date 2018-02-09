3f49890ebb51573a2cbe872a07343677054ff529
albertocovarrubias-at-433224684750 <beto.covarrubias01@gmail.com>
2018-02-09 14:03:18 -0600

    cambio de nombre de archivos

--- WENDYS_FINAL/ExportWENDYS.sh
+++ WENDYS_FINAL/ExportWENDYS.sh
@@ -3,7 +3,7 @@ cd /home/proyectos2/Canada/Wendy/datos
 
 rm *.csv
 hive -e 'set hive.cli.print.header=true; select * from APP_INTELCOMERCIAL_DM.DISTRIBUTION_SUPPLIER_PLANT' | sed 's/[\t]/|/g'  > DISTRIBUTION_SUPPLIER_PLANT.csv
-hive -e 'set hive.cli.print.header=true; select * from APP_INTELCOMERCIAL_DM.DISTRIBUTOR_ACCOUNT_BT_ST' | sed 's/[\t]/|/g'  > DISTRIBUTION_SUPPLIER_PLANT.csv
-hive -e 'set hive.cli.print.header=true; select * from APP_INTELCOMERCIAL_DM.DISTRIBUTOR_ACCOUNT_VF' | sed 's/[\t]/|/g'  > DISTRIBUTION_SUPPLIER_PLANT.csv
+hive -e 'set hive.cli.print.header=true; select * from APP_INTELCOMERCIAL_DM.DISTRIBUTOR_ACCOUNT_BT_ST' | sed 's/[\t]/|/g'  > DISTRIBUTOR_ACCOUNT_BT_ST.csv
+hive -e 'set hive.cli.print.header=true; select * from APP_INTELCOMERCIAL_DM.DISTRIBUTOR_ACCOUNT_VF' | sed 's/[\t]/|/g'  > DISTRIBUTOR_ACCOUNT_VF.csv
 
 echo "-= Finalizo la extracción"
